daaf08d9285cc5bb15289553202b5ec05fca5e3e
reina <reina@192.168.10.103>
2021-06-25 11:44:12 +0900

    最初のコミットです

---
+++ src/main/java/jp/ac/hcs/s3a327/WebpotalApplication.java
@@ -0,0 +1,13 @@
+package jp.ac.hcs.s3a327;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebpotalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebpotalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/ac/hcs/s3a327/WebpotalApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.hcs.s3a327;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebpotalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
