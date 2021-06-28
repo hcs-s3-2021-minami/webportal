6bca3f58c51ed3ee40464d4145231865c77b3d17
reina <reina@LAPTOP-2Q1NR0A9>
2021-06-28 10:05:03 +0900

    WebPortal-メイン機能①

---
+++ src/main/java/jp/ac/hcs/s3a327/main/PortalController.java
@@ -0,0 +1,15 @@
+package jp.ac.hcs.s3a327.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PortalController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+
+}
