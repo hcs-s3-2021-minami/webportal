814204a586676bb88527a0820403a3fd39cfc107
s20193025 <s20193025@HCS19086.stdlan.hcs.local>
2021-10-28 11:49:44 +0900

    デプロイ

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -18,10 +18,3 @@ INSERT INTO task (id, user_id, priority, title, comment, limitday) VALUES (2, 'a
 INSERT INTO task (id, user_id, priority, title, comment, limitday) VALUES (3, 'isida@xxx.co.jp', 'LOW','c', 'それやる', '2020-03-31');
 INSERT INTO task (id, user_id, priority, title, comment, limitday) VALUES (4, 'sano@xxx.co.jp', 'LOW','d', 'どれやる', '2020-03-25');
 INSERT INTO task (id, user_id, priority, title, comment, limitday) VALUES (5, 'abe@xxx.co.jp', 'LOW','e', 'もっとやる', '2020-04-20');
-
-/* 受験報告テーブルのデータ */
-INSERT INTO testreport (id, status, user_name, user_class, attendance_number, day) VALUES (1, '差戻し', '石田悠介', 'S3A3', 01, '2020-03-23');
-INSERT INTO testreport (id, status, user_name, user_class, attendance_number, day) VALUES (2, '承認待', '安部華奈', 'S3A1', 12, '2020-03-24');
-INSERT INTO testreport (id, status, user_name, user_class, attendance_number, day) VALUES (3, '承認待', '石田悠介', 'S3A3', 01, '2020-03-31');
-INSERT INTO testreport (id, status, user_name, user_class, attendance_number, day) VALUES (4, '承認待', '佐野翼', 'S3A2', 35, '2020-03-25');
-INSERT INTO testreport (id, status, user_name, user_class, attendance_number, day) VALUES (5, '差戻し', '安部華奈', 'S3A1', 12, '2020-04-20');
